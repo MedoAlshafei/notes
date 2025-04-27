a36a335dc3ed188cb5ba375ff24c715bdcbe9a90
MedoAlshafei <elshafey009@gmail.com>
2025-04-27 09:55:26 +0300

    fix: correct seed color in NotesApp theme to Cyan

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,12 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        brightness: Brightness.dark,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.cyan,
+          brightness: Brightness.dark,
+        ),
         useMaterial3: true,
       ),
       home: const HomeView(),
