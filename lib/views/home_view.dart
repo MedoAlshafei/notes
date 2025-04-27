12fb02e639d6ada7c773c74c855f2c3d85c35440
MedoAlshafei <elshafey009@gmail.com>
2025-04-27 09:55:40 +0300

    refactor: enhance layout of HomeView with padding and improved text style

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -6,24 +6,29 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'Notes',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-              IconButton(
-                icon: const Icon(Icons.search_rounded),
-                onPressed: () {
-                  // Add your action here
-                },
-              ),
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).padding.top),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Notes',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.search_rounded),
+                  onPressed: () {
+                    // Add your action here
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 70),
+          ],
+        ),
       ),
     );
   }
