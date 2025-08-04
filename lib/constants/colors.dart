e2c0378a3ce9f8935b7be437b32a2fc6b11ef280
MedoAlshafei <elshafey009@gmail.com>
2025-08-04 20:56:56 +0300

    style: update secondary color scheme and add new accent colors for improved UI consistency

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color.fromARGB(255, 216, 216, 216);
 const Color kOnPrimaryColor = Color.fromARGB(255, 173, 173, 173);
-const Color kSecondaryColor = Color.fromARGB(255, 241, 130, 38);
-const Color kSecondaryAccentColor = Color.fromARGB(255, 255, 222, 130);
+const Color kSecondaryColor = Color.fromARGB(255, 209, 153, 0);
+const Color kOnSecondaryColor = Color.fromARGB(255, 0, 0, 0);
+const Color kSecondaryAccentColor = Color.fromARGB(255, 255, 217, 113);
+const Color kOnSecondaryAccentColor = Color.fromARGB(255, 0, 0, 0);
