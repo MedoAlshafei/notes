7240f5595523e5a495e640ee67980a8cb594851d
MedoAlshafei <elshafey009@gmail.com>
2025-08-08 03:01:00 +0300

    Provider Add Note Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes/constants/colors.dart';
 import 'package:notes/constants/strings.dart';
+import 'package:notes/cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:notes/models/note_model.dart';
 import 'package:notes/views/notes_view.dart';
 
@@ -26,25 +28,28 @@ class NotesApp extends StatelessWidget {
       designSize: Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      child: MaterialApp(
-        title: appName,
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          useMaterial3: true,
-          fontFamily: GoogleFonts.poppins().fontFamily,
-          textTheme: GoogleFonts.poppinsTextTheme(),
-          colorScheme: ColorScheme.fromSeed(
-            brightness: Brightness.dark,
-            seedColor: Colors.grey,
-            surface: kPrimaryColor,
+      child: MultiBlocProvider(
+        providers: [BlocProvider(create: (context) => AddNoteCubit())],
+        child: MaterialApp(
+          title: appName,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            useMaterial3: true,
+            fontFamily: GoogleFonts.poppins().fontFamily,
+            textTheme: GoogleFonts.poppinsTextTheme(),
+            colorScheme: ColorScheme.fromSeed(
+              brightness: Brightness.dark,
+              seedColor: Colors.grey,
+              surface: kPrimaryColor,
+            ),
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: Colors.black,
+              foregroundColor: kPrimaryColor,
+              elevation: 4,
+            ),
           ),
-          floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: Colors.black,
-            foregroundColor: kPrimaryColor,
-            elevation: 4,
-          ),
+          home: const NotesView(),
         ),
-        home: const NotesView(),
       ),
     );
   }
