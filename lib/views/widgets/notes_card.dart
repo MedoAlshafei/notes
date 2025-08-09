4dc47fae7a7275bb68458d7ad04ec6eba320bb8a
MedoAlshafei <elshafey009@gmail.com>
2025-08-09 04:32:06 +0300

    Refactor NotesCubit and NotesState to streamline note fetching and state management; update NotesCard and NotesListView to utilize fetched notes.

--- lib/views/widgets/notes_card.dart
+++ lib/views/widgets/notes_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:notes/constants/colors.dart';
+import 'package:notes/models/note_model.dart';
 import 'package:notes/views/edit_note_view.dart';
 
 class NotesCard extends StatelessWidget {
-  const NotesCard({super.key});
+  const NotesCard({super.key, required this.note});
+
+  final NoteModel note;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +19,7 @@ class NotesCard extends StatelessWidget {
       },
       child: Container(
         decoration: BoxDecoration(
-          color: kOnPrimaryColor,
+          color: Color(note.color),
           border: BoxBorder.fromLTRB(
             top: BorderSide(color: Colors.black, width: 1),
             left: BorderSide(color: Colors.black, width: 1),
@@ -31,35 +33,33 @@ class NotesCard extends StatelessWidget {
           children: [
             ListTile(
               title: Text(
-                'Note Title',
-                style: TextStyle(
+                note.title,
+                style: const TextStyle(
                   color: Colors.black,
-                  fontSize: 18.sp,
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               subtitle: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
                 child: Text(
-                  'Note Subtitle',
-                  style: TextStyle(color: Colors.black54, fontSize: 14.sp),
-                  maxLines: 3,
+                  note.subTitle,
+                  style: const TextStyle(color: Colors.black54, fontSize: 14),
+                  // maxLines: 3,
                   overflow: TextOverflow.ellipsis,
                 ),
               ),
               trailing: InkWell(
                 child: IconButton(
                   icon: Icon(Icons.delete, color: Colors.black),
-                  onPressed: () {
-                    // Handle delete action
-                  },
+                  onPressed: () {},
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Text(
-                'May12, 2023',
+                note.date,
                 style: TextStyle(color: Colors.grey[850], fontSize: 12.sp),
               ),
             ),
--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubit/notes_cubit/notes_cubit.dart';
+import 'package:notes/models/note_model.dart';
 import 'package:notes/views/widgets/notes_card.dart';
 
 class NoteListView extends StatelessWidget {
@@ -6,19 +9,26 @@ class NoteListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        physics: BouncingScrollPhysics(),
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: const NotesCard(),
-          );
-        },
-      ),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes =
+            BlocProvider.of<NotesCubit>(context).notes ?? [];
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+            padding: EdgeInsets.zero,
+            physics: BouncingScrollPhysics(),
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: NotesCard(note: notes[index]),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
