9bcc273ea0c926f9e0477fab9248f815e9ec3a52
MedoAlshafei <elshafey009@gmail.com>
2025-08-09 12:00:50 +0300

    Enhance CustomSnackBar by adding padding and increasing text font size for improved readability.

--- lib/views/widgets/custom_snack_bar.dart
+++ lib/views/widgets/custom_snack_bar.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 class CustomSnackBar extends SnackBar {
   CustomSnackBar({super.key, required String text, required Color color})
     : super(
-        content: Text(text, style: const TextStyle(color: Colors.black)),
+        content: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.black, fontSize: 16),
+          ),
+        ),
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
         duration: const Duration(milliseconds: 2000),
         backgroundColor: color,
