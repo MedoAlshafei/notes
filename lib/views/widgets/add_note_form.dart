71a65eae7fe2ae146ec4a53c3eede39a9c48cc6e
MedoAlshafei <elshafey009@gmail.com>
2025-08-09 05:51:13 +0300

    fix date month

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -59,9 +59,9 @@ class _AddNoteFormState extends State<AddNoteForm> {
                     formKey.currentState!.save();
 
                     var currentDate = DateTime.now();
-                    var formattedCurrentDate = DateFormat(
-                      'dd/mm/yyyy',
-                    ).format(currentDate);
+                    var formattedCurrentDate = DateFormat.yMd().format(
+                      currentDate,
+                    );
 
                     var noteModel = NoteModel(
                       title: title!,
